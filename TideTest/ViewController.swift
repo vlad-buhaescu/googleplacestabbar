9f91dc990f4e34fe4e01bccf85d32d1213ec6127
Vlad-Constantin Buhaescu <buhaescuvlad@yahoo.com>
2017-08-09 14:22:09 +0100

    Work in progress: -getiing bars

--- TideTest/ViewController.swift
+++ TideTest/ViewController.swift
@@ -9,37 +9,52 @@
 import UIKit
 import GooglePlaces
 
+
 class ViewController: UIViewController {
     
-    // TODO: Add a button to Main.storyboard to invoke onLaunchClicked.
+    var locationManager:CLLocationManager!
     
-    // Present the Autocomplete view controller when the button is pressed.
-    @IBAction func onLaunchClicked(sender: UIButton) {
-        let acController = GMSAutocompleteViewController()
-        acController.delegate = self
-        present(acController, animated: true, completion: nil)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        determineMyCurrentLocation()
+    }
+    
+    func determineMyCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+        }
+    }
+    
+}
+
+extension ViewController:CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        
+        // manager.stopUpdatingLocation()
+        
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Error \(error)")
     }
 }
 
-extension ViewController: GMSAutocompleteViewControllerDelegate {
-    
-    // Handle the user's selection.
-    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-        print("Place name: \(place.name)")
-        print("Place address: \(place.formattedAddress)")
-        print("Place attributions: \(place.attributions)")
-        dismiss(animated: true, completion: nil)
-    }
-    
-    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
-        // TODO: handle the error.
-        print("Error: \(error)")
-        dismiss(animated: true, completion: nil)
-    }
-    
-    // User cancelled the operation.
-    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
-        print("Autocomplete was cancelled.")
-        dismiss(animated: true, completion: nil)
-    }
-}
